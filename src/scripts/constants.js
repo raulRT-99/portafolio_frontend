a619e17d3de32cae1878bed7aee8d3b5a4200fbf
Raul Reyes <79934182+raulRT-99@users.noreply.github.com>
2025-06-06 11:38:29 -0600

    Update constants.js

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,4 +1,4 @@
 // LOCALHOST URL FOR BACKEND
 //export const BACKEND_URL = 'http://localhost:5000';
 // PRODUCTION URL FOR BACKEND
-export const BACKEND_URL = 'https://backend-portfolio-fernando.onrender.com';
+export const BACKEND_URL = 'https://complete-cherilynn-raulrt-99-3c4f5108.koyeb.app/';
